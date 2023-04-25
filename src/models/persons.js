6ec7ae84e422994331db276c687f17306fb8c369
eduardoChacom <lchacon0@ucol.mx>
2023-04-24 18:25:25 -0600

    Act. Creacion de la app

---
+++ src/models/persons.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose'); // Inyectamos la dependencia de mongoose
+
+// Creamos el esquema de la base de datos para las personas
+let PersonSchema = new mongoose.Schema({
+    nombre: String,
+    edad: Number,
+    tipoSangre: String,
+    nss: String
+});
+
+// Exportamos el modelo de la base de datos
+module.exports = mongoose.model('Persons', PersonSchema);
